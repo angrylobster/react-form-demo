e9eb3211478f2b22f804becc3207ef30a103c9bc
Min Shan <liewminshan@gmail.com>
2022-09-27 18:23:46 +0800

    install eslint

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div style={{ padding: '32px' }}>
-    </div>
-  );
+    return (
+        <div style={{ padding: '32px' }}>
+        </div>
+    );
 }
 
 export default App;
