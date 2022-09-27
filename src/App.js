94d83d678ef11a8218cbe2bb8c20d41fb0f7ecca
Min Shan <liewminshan@gmail.com>
2022-09-27 18:32:33 +0800

    use class based components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-    return (
-        <div style={{ padding: '16px 32px' }}>
-            <h3>My First Form</h3>
-        </div>
-    );
+class App extends React.Component {
+    render() {
+        return (
+            <div style={{ padding: '16px 32px' }}>
+                <h3>My First Form</h3>
+            </div>
+        );
+    }
 }
 
 export default App;
