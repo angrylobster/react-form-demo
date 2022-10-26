d6af3e7d97bcba1c6f6b8fb8b9954579ce370d2b
Min Shan <liewminshan@gmail.com>
2022-10-26 18:04:35 +0800

    fix exports

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
-import { UserForm } from './components';
-import { UserList } from './components/UserList';
+import { UserForm, UserList } from './components';
 
 class App extends React.Component {
     render() {
