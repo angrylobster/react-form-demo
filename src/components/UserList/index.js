c2d2a256682d8d9d181200d0b5da2d067fa31482
Min Shan <liewminshan@gmail.com>
2022-10-29 09:30:53 +0800

    add delete button

--- src/components/UserList/index.js
+++ src/components/UserList/index.js
@@ -6,6 +6,7 @@ export class UserList extends React.Component {
         this.state = {
             users: [
                 {
+                    id: 1,
                     name: 'Banana',
                     age: 42,
                     gender: 'male',
@@ -15,19 +16,32 @@ export class UserList extends React.Component {
             ]
         };
     }
+
+    deleteUser(id) {
+        console.log(`Deleting user with ID ${id}`);
+    }
+
     render() {
         return (
             <>
                 <h3>User List</h3>
-                {this.state.users.map(({ name, age, gender, favoriteFood, description }, index) => (
-                    <div key={index}>
-                        <h4 style={{ marginBottom: 8 }}>{name}</h4>
-                        <div>Age: {age}</div>
-                        <div>Gender: {gender}</div>
-                        <div>Favorite food: {favoriteFood}</div>
-                        <div>Description: {description}</div>
-                    </div>
-                ))}
+                {this.state.users.map(
+                    ({ id, name, age, gender, favoriteFood, description }, index) => (
+                        <div key={index}>
+                            {id !== this.state.isEditing && (
+                                <div>
+                                    <h4 style={{ marginBottom: 8 }}>{name}</h4>
+                                    <div>Age: {age}</div>
+                                    <div>Gender: {gender}</div>
+                                    <div>Favorite food: {favoriteFood}</div>
+                                    <div>Description: {description}</div>
+
+                                    <button onClick={() => this.deleteUser(id)}>Delete</button>
+                                </div>
+                            )}
+                        </div>
+                    )
+                )}
             </>
         );
     }
